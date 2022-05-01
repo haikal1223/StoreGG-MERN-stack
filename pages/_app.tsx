21f7d8de115c26e03ed36c7d2461962cd2b8bc04
haikal1223 <49810289+haikal1223@users.noreply.github.com>
2022-05-01 22:55:35 +0700

    [SGG-14] Converting Member Transactions Detail Pages (#12)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ import '../styles/404-not-found.css';
 import '../styles/overview.css';
 import '../styles/sidebar.css';
 import '../styles/transactions.css';
+import '../styles/transactions-detail.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
