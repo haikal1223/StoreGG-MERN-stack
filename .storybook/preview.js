e614842c58ce1d6fb1837e4dc7285ca18bb5138e
haikal1223 <49810289+haikal1223@users.noreply.github.com>
2022-08-06 17:58:29 +0700

    [SGG-31] Setup Global Style at Storybook (#30)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,20 @@
+import "../styles/utilities.css";
+import "../styles/homepage.css";
+import "../styles/detail.css";
+import "../styles/checkout.css";
+import "../styles/complete-checkout.css";
+import "../styles/sign-in.css";
+import "../styles/sign-up.css";
+import "../styles/sign-up-photo.css";
+import "../styles/sign-up-success.css";
+import "../styles/404-not-found.css";
+import "../styles/overview.css";
+import "../styles/sidebar.css";
+import "../styles/transactions.css";
+import "../styles/transactions-detail.css";
+import "../styles/edit-profile.css";
+import "../styles/navbar-log-in.css";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +23,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,24 @@
 /* eslint-disable react/jsx-props-no-spreading */
 // eslint-disable-next-line linebreak-style
 
-import '../styles/utilities.css';
-import '../styles/homepage.css';
-import '../styles/detail.css';
-import '../styles/checkout.css';
-import '../styles/complete-checkout.css';
-import '../styles/sign-in.css';
-import '../styles/sign-up.css';
-import '../styles/sign-up-photo.css';
-import '../styles/sign-up-success.css';
-import '../styles/404-not-found.css';
-import '../styles/overview.css';
-import '../styles/sidebar.css';
-import '../styles/transactions.css';
-import '../styles/transactions-detail.css';
-import '../styles/edit-profile.css';
-import '../styles/navbar-log-in.css';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
+import "../styles/utilities.css";
+import "../styles/homepage.css";
+import "../styles/detail.css";
+import "../styles/checkout.css";
+import "../styles/complete-checkout.css";
+import "../styles/sign-in.css";
+import "../styles/sign-up.css";
+import "../styles/sign-up-photo.css";
+import "../styles/sign-up-success.css";
+import "../styles/404-not-found.css";
+import "../styles/overview.css";
+import "../styles/sidebar.css";
+import "../styles/transactions.css";
+import "../styles/transactions-detail.css";
+import "../styles/edit-profile.css";
+import "../styles/navbar-log-in.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
